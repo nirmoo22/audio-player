1b5a3ec40192a84d66cd08d22fddf39d434de536
Nirmal Narayanan <nirmal.narayanan04@gmail.com>
2019-09-15 14:49:46 +0530

    Initial commit:
    - Added custom mat theme

---
+++ audio-player-client/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialDesignModule } from './modules/material-design/material-design.module';
+
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    MaterialDesignModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
